81d4690964a824327a42ee85f5bc61d0133011f2
diridari <balz@hm.edu>
2017-04-21 18:23:19 +0200

    bla

--- src/main/java/edu/hm/shareit/resources/SharIt.java
+++ src/main/java/edu/hm/shareit/resources/SharIt.java
@@ -20,6 +20,7 @@ public class SharIt {
     @Consumes("application/json")
     @Produces("application/json")
     public Response doSomthing(SharItBook that){
+        int returnCode = 200;
         JSONObject jsonObject = new JSONObject();
         // System.out.println("Post");
         System.out.println(" Titel: " + that.title + "     Autor: " + that.author + "     ISBN : " + that.isbn);
@@ -27,20 +28,23 @@ public class SharIt {
         if(SharItBook.isValid(that)) {
             if (!SharItBook.exist(that)) {
                 SharItBook.addBook(that);
+                returnCode = 202;
                 //jsonObject.put("detail", "neues buch wurde hinzu gefügt");
                 System.out.println(" add book");
             } else { // das buch gibt es schon
+                returnCode = 226;
                 System.out.println("das buch  " + that +" gibt es schon");
                 jsonObject.put("detail", "es gibt dieses Buch schon");
                 //System.out.println("book exists");
             }
         }
         else {
+            returnCode = 400;
             jsonObject.put("detail", "Ungültige eingabe");
             System.out.println("ungültige eingabe");
         }
 
-        return Response.status(200).entity(jsonObject.toString()).build();
+        return Response.status(returnCode).entity(jsonObject.toString()).build();
     }
 
 
@@ -49,21 +53,34 @@ public class SharIt {
     public Response doElse(){
         //  System.out.println("GET");
         JSONObject jsonObject = new JSONObject();
-
         Iterator<SharItBook> allBooks = SharItBook.getAllBooks();
+
         if(!allBooks.hasNext())
             jsonObject.put("detail","Es gibt noch keine bücker");
         else{
             while(allBooks.hasNext()){
                 SharItBook t = allBooks.next();
-                jsonObject.accumulate("book",t.toJSON()); // add to respond
+                jsonObject.accumulate("books",t.toJSON()); // add to respond
                 System.out.println("add to respond : " + t.title); // test
             }
 
         }
-       // System.out.println("respond : " + jsonObject.toString());
+
+        System.out.println("respond : " + jsonObject.toString());
         return Response.status(200).entity( jsonObject.toString()).build();
     }
 
 
 }
+/*
+ if(!allBooks.hasNext())
+            jsonObject.put("detail","Es gibt noch keine bücker");
+        else{
+            while(allBooks.hasNext()){
+                SharItBook t = allBooks.next();
+                jsonObject.accumulate("books",t.toJSON()); // add to respond
+                System.out.println("add to respond : " + t.title); // test
+            }
+
+        }
+ */
