656babb18c126789d9cec7da502ca6ec2e38cc63
Matthias Herpers <herpers.matthias0@hm.edu>
2017-04-27 10:25:37 +0200

    MediaServiceResult done?

--- src/main/java/edu/hm/shareit/resources/MediaServiceResult.java
+++ src/main/java/edu/hm/shareit/resources/MediaServiceResult.java
@@ -1,23 +1,30 @@
 package edu.hm.shareit.resources;
 
-import com.sun.org.apache.xerces.internal.util.Status;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 /**
  * Created by MatHe on 26.04.2017.
  */
 public enum MediaServiceResult {
 
-    OK,
-    second,
-    good;
+    OK(200, Status.OK),
+    Failed(400, Status.BAD_REQUEST),
+    conflict(409,Status.CONFLICT);
+
+    private final int id;
 
     int getCode(){
-        return 0;
+        return this.id;
+    }
+    private final Status status;
+    Status getStatus(){
+        return this.status;
     }
 
-    Status getStatus(){
-        return null;
-    }
+    MediaServiceResult(int id,Status state) { this.id = id; this.status=state;}
+
+
 
     /*public static MediaServiceResult valueOf(String string){
         return null;
