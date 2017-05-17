d8bf053f85885c7307a87f27a38d845280442754
Matthias Herpers <herpers.matthias0@hm.edu>
2017-05-17 14:18:34 +0200

    start

---
+++ src/main/java/edu/hm/shareit/authorization/AuthServer.java
@@ -0,0 +1,71 @@
+package edu.hm.shareit.authorization;
+
+
+    import org.json.JSONArray;
+    import org.json.JSONObject;
+
+    import javax.ws.rs.*;
+    import javax.ws.rs.core.Response;
+    import java.util.HashMap;
+    import java.util.Map;
+
+/**
+ * Created by MatHe on 17.05.2017.
+ */
+@Path("/authorization")
+public class AuthServer {
+
+    public AuthServer(){
+        System.out.println("HAHAHAHA");
+    }
+
+    Map<String,String> database = new HashMap<>();
+
+    @POST
+    @Path("/login")
+    @Consumes("application/json")
+    @Produces("application/json")
+    public Response login(JSONObject jsonObject){
+        System.out.println(jsonObject.toString());
+
+        JSONObject myResponse = new JSONObject();
+
+        myResponse.put("code","200");
+        String newToken = "asdfas";
+        myResponse.put("Token",newToken);
+        database.put(newToken,"name=asdf,info=asdfgs2");
+
+        return Response.status(400).entity(myResponse.toString()).build();
+
+    }
+
+    @POST
+    @Path("/signup")
+    //@Consumes("application/json")
+    @Produces("application/json")
+    public Response signup(){
+        System.out.println("####################");
+        return Response.status(400).build();
+
+    }
+
+    @POST
+    @Path("/logout")
+    @Consumes("application/json")
+    @Produces("application/json")
+    public Response logout(){
+        return Response.status(400).build();
+    }
+
+
+    @POST
+    @Path("/validate")
+    @Consumes("application/json")
+    @Produces("application/json")
+    public Response validate(){
+        return Response.status(400).build();
+    }
+
+
+
+}
